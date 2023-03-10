fe6d7f5849c8ad548ba81bb1b28d087b86e99b0f
Tema Lapin <Smoke52ru@ya.ru>
2023-03-10 23:38:53 +0300

    first commit

--- main.py
+++ main.py
@@ -22,22 +22,23 @@ def sign_data(data_file_path, signer_name, signer_private_key_file_path, signer_
     # Создать сертификат подписи
     certificate = create_certificate(signer_name, signer_public_key_file_name)
     # Создать папку для файлов подписи
-    if os.path.exists("Digital signature"):
-        shutil.rmtree("Digital signature")
-    os.mkdir("Digital signature")
+    directory = "sign"
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.mkdir(directory)
     # Записать зашифрованный хеш в файл и поместить в специальную папку для ЭЦП
-    with open("Digital signature/Digital signature.pem", "wb") as f:
+    with open(directory + "/sign.pem", "wb") as f:
         f.write(encrypted_hash)
         # [f.write(x) for x in (encrypted_hash, certificate)]
     # Добавить сертификат
-    with open("Digital signature/Digital signature certificate.pem", "a") as f:
+    with open(directory + "/cert.pem", "a") as f:
         f.write(certificate)
     # Скопировать файл с данными в специальную папку для ЭЦП
     data_file_name = os.path.basename(data_file_path)
-    shutil.copyfile(data_file_path, "Digital signature/" + data_file_name)
+    shutil.copyfile(data_file_path, directory + "/" + data_file_name)
     # Скопировать файл с открытым ключом отправителя в специальную папку для ЭЦП
-    shutil.copyfile(signer_public_key_file_path, "Digital signature/" + signer_public_key_file_name)
-    os.rename("Digital signature/" + signer_public_key_file_name, "Digital signature/" + "Signer public key.pem")
+    shutil.copyfile(signer_public_key_file_path, directory + "/" + signer_public_key_file_name)
+    os.rename(directory + "/" + signer_public_key_file_name, directory + "/" + "pubkey.pem")
 
 
 # Зашифровать хеш
@@ -117,39 +118,43 @@ def generate_keys():
 
 # Сохранить закрытый и открытый ключи в файлы
 def save_keys(private_key, public_key):
-    file = open("Keys/private_key.pem", "wb")
+    file = open("private_key.pem", "wb")
     file.write(private_key)
     file.close()
-    file = open("Keys/public_key.pem", "wb")
+    file = open("public_key.pem", "wb")
     file.write(public_key)
     file.close()
 
 
 if __name__ == '__main__':
-    print("1. Сгенерировать ключи\n2. Подписать данные\n3. Проверить подпись\n(1/2/3)")
-    mode = input(">")
-    if mode == str(1):
-        private_key, public_key = generate_keys()
-        save_keys(private_key, public_key)
-        print("Ключи сгенерированы.")
-    elif mode == str(2):
-        data_file_path = input("Введите путь к файлу с данными: ")
-        private_key_path = input("Введите путь к файлу с закрытым ключом подписывающего: ")
-        public_key_path = input("Введите путь к файлу с открытым ключом подписывающего: ")
-        signer_name = input("Введите ФИО подписывающего: ")
-        sign_data(data_file_path,
-                  signer_name,
-                  private_key_path,
-                  public_key_path)
-        print("\nДанные успешно подписаны.")
-    elif mode == str(3):
-        data_file_path = input("Введите путь к файлу с данными: ")
-        digital_signature_path = input("Введите путь к файлу с электронной подписью: ")
-        public_key_path = input("Введите путь к файлу с открытым ключом подписывающего: ")
-        verification = verification_of_data_digital_signature(data_file_path,
-                                               digital_signature_path,
-                                               public_key_path)
-        if verification:
-            print("\nВерификация успешна. Хеши равны. Данные не были изменены или подменены.")
+    while True:
+        print("1. Сгенерировать ключи\n2. Подписать данные\n3. Проверить подпись\n(1/2/3)")
+        mode = input("> ")
+        if mode == str(1):
+            private_key, public_key = generate_keys()
+            save_keys(private_key, public_key)
+            print("Ключи сгенерированы.")
+        elif mode == str(2):
+            data_file_path = input("Введите путь к файлу с данными: ")
+            private_key_path = input("Введите путь к файлу с закрытым ключом подписывающего: ")
+            public_key_path = input("Введите путь к файлу с открытым ключом подписывающего: ")
+            signer_name = input("Введите ФИО подписывающего: ")
+            sign_data(data_file_path,
+                      signer_name,
+                      private_key_path,
+                      public_key_path)
+            print("\nДанные успешно подписаны.")
+        elif mode == str(3):
+            data_file_path = input("Введите путь к файлу с данными: ")
+            digital_signature_path = input("Введите путь к файлу с электронной подписью: ")
+            public_key_path = input("Введите путь к файлу с открытым ключом подписывающего: ")
+            verification = verification_of_data_digital_signature(data_file_path,
+                                                                  digital_signature_path,
+                                                                  public_key_path)
+            if verification:
+                print("\nВерификация успешна. Хеши равны. Данные не были изменены или подменены.")
+            else:
+                print("\nВнимание! Верификация провалилась. Хеши не равны. Данные, вероятно, не соответствуют")
+
         else:
-            print("\nВнимание! Верификация провалилась. Хеши не равны. Данные, вероятно, не соответствуют")
+            break;
